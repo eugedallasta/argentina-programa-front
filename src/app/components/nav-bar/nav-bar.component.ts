02575d07f5a38e269aff62e9f0af5d50f8feba74
Eugenia Dallasta <mariaedallasta@gmail.com>
2022-05-27 17:24:49 -0300

    add ng toastr

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  userLogged = this.loginService.getUserLogged();
+
+  constructor(private loginService:LoginService, private toastr:ToastrService) { }
 
   ngOnInit(): void {
   }
+
+  onLogOut(){
+    this.loginService.logout();
+  }
+
+  showPhone() {
+    this.toastr.info('15 460 7552', 'Phone', {positionClass: 'toast-top-right',tapToDismiss: true,} );
+  }
+  showEmail() {
+    this.toastr.info('mariaedallasta@gmail.com', 'Email', {
+    positionClass: 'toast-top-right',
+    tapToDismiss: true,
+    });
+  }
+
+
 
 }
