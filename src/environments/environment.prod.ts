8c09983ebba0e455ae4b472f2788820c01729b48
Eugenia Dallasta <mariaedallasta@gmail.com>
2022-05-28 14:52:43 -0300

    environment production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBaseUrl: 'https://eugenia-back.herokuapp.com/',
+  firebaseConfig : {
+    apiKey: "AIzaSyAYSPuJi9yg70wSp7Z2qvRS6DqfGX0NIok",
+    authDomain: "portfolioauth-8eb63.firebaseapp.com",
+    projectId: "portfolioauth-8eb63",
+    storageBucket: "portfolioauth-8eb63.appspot.com",
+    messagingSenderId: "451882293600",
+    appId: "1:451882293600:web:9aef0b2630e48ac4d92605",
+    measurementId: "G-LZ5QGNYX9X"
+  }
 };
